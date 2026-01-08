516ceeaf6ba4a4df860661aa05451733c6408cdc
Dat <copfreund@gmail.com>
2026-01-08 16:19:08 +0100

    Add parameter file creation for downloaded PDB
    Added functionality to create a parameter text file with the PDB ID.

--- download_pdb.py
+++ download_pdb.py
@@ -1,20 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-import os
-import urllib.request
-
-# ------------------------------------------------------------------------------
-# 1. Download structure
-# ------------------------------------------------------------------------------
-
-
 def download_pdb_file(pdb_id, output_dir="."):
     """Download PDB file"""
     print("\n=== Downloading PDB file ===")
@@ -24,6 +7,10 @@ def download_pdb_file(pdb_id, output_dir="."):
     pdb_file = f"{protein_directory}/{pdb_id}.pdb"
     url = f"https://files.rcsb.org/download/{pdb_id}.pdb"
 
+    # Create parameter text file
+    with open("params.json", "w") as param_file:
+        param_file.write('{"pdb_id": "' + pdb_id + '"}\n')
+
     try:
         urllib.request.urlretrieve(url, pdb_file)
         print(f"Downloaded: {pdb_file}")
@@ -31,17 +18,3 @@ def download_pdb_file(pdb_id, output_dir="."):
     except Exception as e:
         print(f"Download error: {e}")
         exit()
-
-
-# ------------------------------------------------------------------------------
-# Entry point
-# ------------------------------------------------------------------------------
-
-if __name__ == "__main__":
-    pdb_id = input("Enter desired PDB code to be downloaded from RCSB: ")
-    #pdb_id = "4OHU"
-    #pdb_id = os.getenv("PARAM_PDB_ID")
-    #protein_directory = os.getenv("PARAM_PROTEIN_DIRECTORY")
-    protein_directory = "molecular_docking/protein_files"
-    download_pdb_file(pdb_id)
-
